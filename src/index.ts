3d6556de13dfe44319028079699c2b04635c7908
miguel.chauca@almundo.com <miguel.chauca@almundo.com>
2018-02-20 21:48:11 -0300

    eliminando dist

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,14 @@
 import * as express from 'express';
+import * as bodyParser from 'body-parser';
 import { routers } from './router'
 
 const app: express.Express = express();
 
-app.use('/',routers())
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
+app.use(bodyParser.text());
+
+app.use('/', routers())
 
 app.listen(5000, () => {
     console.log("render en puerto 5000");
