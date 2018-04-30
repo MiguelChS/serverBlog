6540aaf8c9b8898050657caf495330e251fa5494
Joaquin Noe <joako.n96@gmail.com>
2018-04-30 01:37:26 -0300

    merge into develop

--- src/server/index.ts
+++ src/server/index.ts
@@ -3,19 +3,19 @@ import * as bodyParser from 'body-parser';
 import { routers } from './router/index';
 import { join } from 'path';
 import { status404 } from './middleware/index';
+import { connect } from "mongoose";
 
 const app: express.Express = express();
-
+connect('mongodb://localhost/posts-escuelita')
 
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 app.use(bodyParser.text());
 //bundle
-app.use("/dist",express.static(join(__dirname, '..', '..', 'dist/client')));
-app.use("/",express.static(join(__dirname, '..', '..', 'public')));
-app.use("/bootstrap",express.static(join(__dirname, '..', '..', 'node_modules/bootstrap/dist')));
-app.use("/font-awesome",express.static(join(__dirname, '..', '..', 'node_modules/font-awesome')));
-
+app.use("/dist", express.static(join(__dirname, '..', '..', 'dist/client')));
+app.use("/", express.static(join(__dirname, '..', '..', 'public')));
+app.use("/bootstrap", express.static(join(__dirname, '..', '..', 'node_modules/bootstrap/dist')));
+app.use("/font-awesome", express.static(join(__dirname, '..', '..', 'node_modules/font-awesome')));
 
 app.use('/', routers())
 
--- src/server/router/service/service.api.ts
+++ src/server/router/service/service.api.ts
@@ -1,25 +1,25 @@
-import { IPagePost, enumTypePost,dataResponse } from './service.page.interface';
-import {IPostAPI, PostAPI} from 'posts-escuelita';
+import { IPagePost, enumTypePost } from './service.page.interface';
+import { IPostAPI, PostAPI } from 'posts-escuelita';
 
 const mock: Array<IPagePost> = [];
 const postAPI = PostAPI();
 
 for (let i = 0; i < 23; i++) {
-    mock.push({
-        urlImage: "/images/perfil2.jpeg",
+    postAPI.create({
+        urlImage: "http://demo.shapedtheme.com/kotha-pro-html/assets/images/post-thumb-1.jpg",
         dateString: "Octubre 13, 2017",
         detalle: {
             categoria: `${i}`,
             descripcion: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua...",
             title: "ADVENTURE TO TRAVEL LONELY",
             linkPost: "/post/TestHash001",
-            typePost: enumTypePost.PREPOST
+            //typePost: enumTypePost.PREPOST
         }
     })
 }
 
 export async function getListPost(category: string): Promise<Array<any> | null> {
-    return mock;
+    return postAPI.read();
 }
 
 const mockbyID: dataResponse = {
